a9e526634bb138af5302afdf1d68990254fbf333
DarkoBorojevic <info@tipoit.net>
2024-07-07 14:20:28 +0200

    Creating HomeView, ProductView views and Product component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
+import HomeView from './views/HomeView.jsx';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Header />
         <main className='py-3'>
           <h1>Redux tutorial</h1>
+          <HomeView />
         </main>
         <Footer />
       </div>
