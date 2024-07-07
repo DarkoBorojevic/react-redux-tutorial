d5538457cc18a20c74c6869fb71852b6741c4c81
DarkoBorojevic <info@tipoit.net>
2024-07-07 14:53:24 +0200

    Implementing router in index.js in frontend.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { 
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomeView from './views/HomeView';
+
+const myRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeView />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={myRouter} />
   </React.StrictMode>
 );
 
