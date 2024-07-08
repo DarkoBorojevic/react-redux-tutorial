4607bd70bb0772757c24e36ab6827b730d9d99b7
DarkoBorojevic <info@tipoit.net>
2024-07-08 07:18:05 +0200

    Adding and implementing ProductView in /views.

--- frontend/src/views/ProductView.jsx
+++ frontend/src/views/ProductView.jsx
@@ -1,8 +1,84 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { 
+  Row, 
+  Col, 
+  Image, 
+  ListGroup, 
+  Card,
+  ListGroupItem
+} from 'react-bootstrap';
+// import products from "../products";
 
 const ProductView = () => {
+  const [product, setProduct] = useState({});
+
+  const { id: productId } = useParams();
+  // const product = products.find((prod) => prod._id === productId);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    }
+
+    fetchProduct();
+  }, [productId]);
+
+  // console.log(product);
+
   return (
-    <div>ProductView</div>
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+      <Row>
+        <Col md={5}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={4}>
+          <ListGroup variant='flush'>
+            <ListGroupItem>
+              <h3>{product.name}</h3>
+            </ListGroupItem>
+            <ListGroupItem>
+              <strong>Price: $ {product.price}</strong>
+            </ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroupItem>
+                {product.description}
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>
+                    Price:
+                  </Col>
+                  <Col>
+                    <strong>$ {product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>
+                    Status:
+                  </Col>
+                  <Col>
+                    <strong>{product.countInStock > 0 ? 'In Stock.' : 'Out of Stock.'}</strong>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
   )
 }
 
